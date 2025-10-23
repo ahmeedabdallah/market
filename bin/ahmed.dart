6a2b7a20b4befa9288a1713cb20d00dc9deca4af
ahmeedabdallah <ahmeedabdallllah@gmail.com>
2025-10-23 13:29:25 -0700

    Fixed product update logic and improved UI

---
+++ bin/ahmed.dart
@@ -0,0 +1,11 @@
+void main (){
+}
+class Product {
+  String name;
+  int quantity;
+  double price;
+  int id;
+  Product(this.id,this.name,this.quantity,this.price);
+  @override
+  String nnn()=>"id : $id  | name : $name | quantity : $quantity | price : $price";
+}
